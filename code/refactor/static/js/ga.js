6a4e8b7ebfe4da3ba75ac9c1f9e0cd249e1e1552
Ben G <beg5670@gmail.com>
2013-11-10 01:49:45 -0500

    dynamically calculate fitness score and normalize it

--- code/refactor/static/js/ga.js
+++ code/refactor/static/js/ga.js
@@ -1,98 +1,109 @@
-// function getRandomInt (min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
+var original_notes = [];
+var adjusted_order = [];
+var reversed_original_notes; // used to calculate max possible euclidean distance
 
-// $(function() {
-//     var settings = {};
-//     console.log('hello');
-//     // var rnd = getRandomInt(1, 5);
-//     // if(rnd < 2) {
-//     //     $("#hello > p#ben").html("<p>hahaha</p>");
-//     // } else {
-//     //     $.getJSON('/individual/0/0', {song: 'winter_allegro', artist: 'vivaldi'}, function(o) {
-//     //         console.log(o.note);
-//     //         o.note.forEach(function(el) {
-//     //             $("#hello").append("<p>"+el+"</p>");
-//     //         });
-//     //     })
-//     // }
+var colors = {
+    'A': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+    'B': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+    'C': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+    'D': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+    'E': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+    'F': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+    'G': '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+};
 
-//     $.post('/initialize', {num_gen: 3, isize: 3, psize: 3, mc_nodes: 2, mc_size: 100, artist: 'vivaldi', song: 'winter_allegro'}, function(o) {
-//         settings = o.settings;
-//         var cg = 0;
-//         var ci = 0;
-//         console.log('psize is ', settings);
-//         $.post('/spawn', {artist: 'vivaldi', song: 'winter_allegro', isize: 3, psize: 2, mc_size: 5, mc_nodes: 2}, function(pop) {
-//             console.log(pop);
-//             $.getJSON('/individual/' + cg + '/' + ci, {song: 'winter_allegro', artist: 'vivaldi'}, function(o) {
-//                 ci++;
-//                 console.log(o.note);
-//                 o.note.forEach(function(el) {
-//                     $("#hello").append("<p>"+el+"</p>");
-//                 });
-//                 $('#next').click(function() {
-//                     console.log("ci is " + ci);
-//                     if(ci <= settings.psize) {
-//                         console.log("ci is less than pop size");
-//                         $.getJSON('/individual/' + cg + '/' + ci, {song: 'winter_allegro', artist: 'vivaldi'}, function(o) {
-//                             o.note.forEach(function(el) {
-//                                 $("#hello").append("<p>"+el+"</p>");
-//                             });
-//                         })
-//                     } else{
-//                         // console.log('ci is ' + ci);
-//                         console.log("next gen now!!!");
-//                         console.log('cg is '+ cg);
-//                     }
-//                     ci++;
-//                     console.log("new ci is " + ci);
-//                 })
-//                 // o.note.forEach(function(el) {
-//                 //     $("#hello").append("<p>"+el+"</p>");
-//                 // });
-//             })
-//         })
-//     });
+Individual = function(r) {
+    this.id = r.id;
+    this.generation = r.generation;
+    this.score = r.fitness || 0;
+    this.notes = r.notes;
+};
 
-//     // http://localhost:5000/spawn -d "artist=vivaldi&song=winter_allegro&isize=3&psize=2&mc_size=5&mc_nodes=2"
-// });
-original_notes = [];
+Individual.prototype.color = function(pitch) {
+    return "background-color:".concat(colors[pitch], ";");
+};
+
+Individual.prototype.render_style = function(pitch) {
+    return "border:2px solid black;".concat(this.color(pitch));
+};
+
+Individual.prototype.render_notes = function() {
+    var that = this;
+    var _final = [];
+    this.notes.forEach(function(el) {
+        $("<div id="+el.index+">").appendTo("#sortable-trait");
+        el.notes.forEach(function(_note) {
+            $("<div/>", {
+                id: el.index,
+                class: _note,
+                text: _note,
+                style: that.render_style(_note[0]),
+            }).appendTo("#" + el.index);
+        });
+    });
+};
+
+Individual.prototype.fitness = function() {
+    if(adjusted_order.length < 1) {
+        console.log("fitness is 100");
+    }
+
+    var _score = euclidean_distance(original_notes, adjusted_order);
+    var _max_score = euclidean_distance(original_notes, reversed);
+    var _normalized = normalize(_score, 0, _max_score);
+    $('#current_score').html("Current Score: <small>" + _normalized + "/100</small>");
+    return _normalized;
+};
+
+function normalize(score, min, range) {
+    /*
+        we need to normalize the score so we can operatore on a scale between
+        0 and 100.
+
+        additionally, if you dont move the notes around then your score is 0,
+        but it should be displayed as 100.
+    */
+    return (100 - (((score - min) / range) * 100));
+}
+
+function euclidean_distance(x, y) {
+    var _score = 0;
+    x.forEach(function(el) {
+        _score += Math.sqrt(Math.pow((el.index - y[el.index].index), 2));
+    });
+
+    return _score;
+}
 
 function initializeMusic() {
-        MIDI.loadPlugin({
-                soundfontUrl: "../../static/js/soundfont/",
-                instrument: ["acoustic_grand_piano"],
-                callback: playMusic
-        });
+    MIDI.loadPlugin({
+        soundfontUrl: "../../static/js/soundfont/",
+        instrument: ["acoustic_grand_piano"],
+        callback: playMusic
+    });
 }
 
 function playMusic() {
-    playSong(original_notes);
+    var _chords = adjusted_order.length > 0 ? adjusted_order : original_notes;
+    playSong(_chords);
 }
 
-function playSong(keys) {
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
+function playSong(chords) {
+    for (var i = 0; i < chords.length; i++) {
+        var chord = chords[i];
         var delay = i / 4;
-        if (typeof(key) == 'string')
-            playKey(key, delay);
-        else
-            playChord(key, delay);
+        playChord(chord, delay);
     }
 }
 
-function playKey(key, delay) {
-        var midiNote = MIDI.keyToNote[key];
-        console.log(key + ' (' + midiNote + ') ' + delay);
-        MIDI.noteOn(0, midiNote, 127, delay);
-}
 
-function playChord(keys, delay) {
-    console.log(keys.notes);
+function playChord(chord, delay) {
     var midiNotes = [];
-    for (var i = 0; i < keys.notes.length; i++)
-        midiNotes.push(MIDI.keyToNote[keys.notes[i]]);
-    console.log(keys.notes + ' (' + JSON.stringify(midiNotes) + ') ' + delay);
+
+    for (var i = 0; i < chord.notes.length; i++) {
+        midiNotes.push(MIDI.keyToNote[chord.notes[i]]);
+    }
+    console.log(chord.notes + ' (' + JSON.stringify(midiNotes) + ') ' + delay);
     MIDI.chordOn(1, midiNotes, 127, delay);
 }
 
@@ -100,7 +111,7 @@ $(function() {
     var indi_id = $("#indi-id").attr("class");
     var generation = $("#current-gen").attr("class");
     var indi_uri = '/individual/' + generation + '/' + indi_id;
-
+    var individual;
     $.getJSON(indi_uri, function(resp) {
         resp.notes.forEach(function(el) {
             var idx = el[0];
@@ -110,18 +121,41 @@ $(function() {
                 'notes': _notes
             });
         });
+        resp.notes = original_notes;
+        reversed = original_notes.reverse();
+        individual = new Individual(resp);
+        individual.render_notes();
     });
 
-
-    $('#listen').click(function(){
+    $('#play').click(function(){
         initializeMusic();
-        $.post(indi_uri, {fitness: 100}, function(resp) {
-            console.log("resp is ", resp);
-        });
     });
 
     $("#next-song").click(function() {
-        console.log("CLICK!!!");
         $("#ns").submit();
     });
+
+    $( "#sortable-trait" ).sortable({
+        stop: function(event) {
+              adjusted_order.length = 0;
+              $('div#sortable-trait > div').each(function(index) {
+                  var child_notes = [];
+                  $(this).children().each(function(idx) {
+                      child_notes.push($(this).attr('class'));
+                  });
+                  adjusted_order.push({
+                      index: $(this).attr('id'),
+                      notes: child_notes
+                  });
+              });
+              var _score = individual.fitness();
+              console.log("normalized score is " + _score);
+              initializeMusic();
+              $.post(indi_uri, {fitness: _score}, function(resp) {
+                  console.log("resp is ", resp);
+              });
+        }
+    });
+
+    $( "#sortable-trait" ).disableSelection();
 });
