78b8093456aa5a1b4514202b1f357033310bb6ff
Ben G <beg5670@gmail.com>
2013-12-09 17:53:29 -0500

    use gevent wsgi instead of flask server

--- code/runserver.py
+++ code/runserver.py
@@ -1,6 +1,11 @@
+import gevent.monkey
+from gevent.pywsgi import WSGIServer
+gevent.monkey.patch_all()
+
 from gor0x import create_app
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
-    app.run()
+    http_server = WSGIServer(('0.0.0.0', 8888), app)
+    http_server.serve_forever()
