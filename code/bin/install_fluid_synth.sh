d84fe04e1c3d5d37a2452d21644ab74d897a6a81
Ben G <beg5670@gmail.com>
2013-12-09 20:12:12 -0500

    shell script to install fluidsynth bc mavericks doesnt have midi support

---
+++ code/bin/install_fluid_synth.sh
@@ -0,0 +1,10 @@
+# OS X Mavericks does not come with MIDI support. These need to be
+# installed in order to get MIDI support, although, I am unable to even
+# play a MIDI file with this...
+
+brew install fluidsynth
+wget http://www.schristiancollins.com/soundfonts/GeneralUser_GS_1.44-FluidSynth.zip
+unzip GeneralUser_GS_1.44-FluidSynth.zip
+mkdir -p /usr/local/share/fluidsynth
+mv GeneralUser\ GS\ 1.44\ FluidSynth/GeneralUser\ GS\ FluidSynth\ v1.44.sf2 /usr/local/share/fluidsynth
+fluidsynth -i /usr/local/share/fluidsynth/generaluser.v.1.44.sf2 file.mid
