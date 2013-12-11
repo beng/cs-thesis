340576a1db7b9911266726d5a1499c9c9dc56564
Ben G <beg5670@gmail.com>
2013-12-10 21:09:52 -0500

    update installation script

--- code/setup.sh
+++ code/setup.sh
@@ -1,3 +1,16 @@
+# # download and install redis
+cd ./bin/
+wget http://download.redis.io/releases/redis-2.8.2.tar.gz
+tar xzf redis-2.8.2.tar.gz
+cd redis-2.8.2
+make
+cd ../
+cp redis.conf ./redis-2.8.2
+rm -rf redis-2.8.2.tar.gz
+cd ../
+
+# setup virtual environment and install third party libraries
+sudo pip install virtualenv
 VPATH=`dirname $0`/virtual
 mkdir -p $VPATH
 virtualenv --distribute --python=`which python2.7` $VPATH
